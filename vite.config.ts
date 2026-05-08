bd59a4847282eacf4cba9dd7d3fdaa38f40ab99b
RoyOnofre <royoa.15@gmail.com>
2026-05-08 03:40:57 +0000

    confirma

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, '.', '');
+const isBuild = command === 'build';
   return {
-      base: '/TechStore-Manager/',   // ← importante: con barras al inicio y final
+   base: isBuild ? '/TechStore-Manager/' : '/',   // ← condicional
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
