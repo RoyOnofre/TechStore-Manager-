0c27a0625d10a27d87b38333ffb255029a86b4c1
RoyOnofre <royoa.15@gmail.com>
2026-05-08 03:35:20 +0000

    confirmar

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+      base: '/TechStore-Manager/',   // ← importante: con barras al inicio y final
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
